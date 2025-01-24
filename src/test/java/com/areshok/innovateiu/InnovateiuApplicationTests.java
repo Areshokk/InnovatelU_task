495268b56a39b16ceff1d2c64166bf6c5b3e666b
Yura <67182548+Areshokk@users.noreply.github.com>
2025-01-24 17:44:05 +0200

    implemented methods

---
+++ src/test/java/com/areshok/innovateiu/InnovateiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areshok.innovateiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovateiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
